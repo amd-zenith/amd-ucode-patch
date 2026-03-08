14376eb41fdee99b9579002c95faddb6c5fc6817
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-03-08 15:10:05 +0100

    Patch: Parse signature and public key

--- src/amd_ucode_patch/structures/ucode_patch.py
+++ src/amd_ucode_patch/structures/ucode_patch.py
@@ -7,10 +7,15 @@ from .ucode_patch_header import UcodePatchHeader
 @dataclass
 class UcodePatch:
     header: UcodePatchHeader
+    signature: bytes
+    public_key: bytes
+
 
     @staticmethod
     def from_bytes(buf: bytes) -> "UcodePatch":
         header = UcodePatchHeader.from_bytes(buf)
         return UcodePatch(
-            header,
+            header=header,
+            signature=buf[32:288],
+            public_key=buf[288:800],
         )
